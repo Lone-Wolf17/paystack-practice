e163649d40aa7f6b838f9ef461ee916ed01e2165
lonewolf <adeyanju.akorede1@gmail.com>
2021-09-05 00:57:27 +0100

    ** Implement Donor Model

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ connectDB();
 // Routers
 
 
+const port =  process.env.PORT || 3000
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${3000}`);
---
+++ constants/modelNames.js
@@ -0,0 +1,3 @@
+module.exports = {
+    DonorModelName: 'Donor'
+}
